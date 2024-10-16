1ad5f8763365932b95d12fd934a45de904e45457
vi3m <dev.vijaymlv@gmail.com>
2024-10-16 18:03:24 +0530

    fixed linting

--- entry.py
+++ entry.py
@@ -1,9 +1,11 @@
-# print("Loading Model for inference")
-# print("Infered")
-# print("Predictions")
+"""Using this function to test Github Actions Workflows for Python
+"""
+
 
 def add(a, b):
+    """Just add two given numbers"""
     return a + b
+
 
 print("Done!")
 print("Exit after deploy.")
